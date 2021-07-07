4c997468edeb7d88afe56ce553844f0979ba7920
Pierre Ribault <pribault@svp.com>
2021-07-07 20:19:55 +0200

    Prepare aggregation script

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const fs = require('fs')
 const { Client } = require('@elastic/elasticsearch');
 const Twitter = require('twitter');
 const twitter = new Twitter({
@@ -9,19 +10,90 @@ const twitter = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 })
 
-const elastic = new Client({ node: 'http://localhost:9200' })
+const elastic = new Client({
+    cloud: {
+        id: process.env.ELK_HOST,
+    },
+    auth: {
+        apiKey: process.env.ELK_SECRET
+    }
+})
 
-const stream = twitter.stream('statuses/filter', {track: 'BTC'});
+function log(message) {
+    fs.appendFile('log.txt', `${message}\n`, function (err) {
+        if (err) throw err;
+    });
+}
 
-stream.on('data', async function(event) {
-    await elastic.index({
-        index: 'tweets',
-        body: event
-      })    
-});
+function search(query, id = 0) {
+    const body = {
+        q: query,
+        lang: 'eu',
+        count: '100',
+    }
 
-stream.on('error', function(error) {
-  throw error;
-});
-                
-	        
+    if (id !== 0) {
+        body.max_id = id + 1
+    }
+
+    return twitter.get('search/tweets', body);
+}
+
+async function aggregate(query) {
+    log(`${query}: Start`)
+
+    let lastId = 0
+    let page = 0
+
+    while (true) {
+        let tweets = search(query, lastId)
+        let end = false
+
+        tweets.then(response => {
+            if(response.statuses === []) {
+                end = true
+            } else {
+                for (const tweet of response.statuses) {
+                    lastId = tweet.id
+
+                    elastic.index({
+                        index: 'tweets',
+                        body: {
+                            "date": tweet.created_at,
+                            "text": tweet.text,
+                            "user": tweet.user.id,
+                            "user_name": tweet.user.name
+                        }
+                    })
+                }
+            }
+        })
+
+        if (end) {
+            break
+        }
+
+        if (page === 10) {
+            log(`${query}: Waiting 16 minutes, actual page: ${page}`)
+            await require('timers/promises').setTimeout(960000);
+            log(`${query}: Waiting ended, retake the process.`)
+        }
+
+        page += 1
+    }
+
+    return  {message: `${query}: import ended!`}
+}
+
+aggregate('BTC')
+    .then(data => log(data.message))
+aggregate('ETH')
+    .then(data => log(data.message))
+aggregate('DOGE')
+    .then(data => log(data.message))
+aggregate('BNB')
+    .then(data => log(data.message))
+aggregate('LINK')
+    .then(data => log(data.message))
+aggregate('ADA')
+    .then(data => log(data.message))
